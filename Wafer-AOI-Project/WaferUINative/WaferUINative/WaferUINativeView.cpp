9a0303eda4bc71432526e3e728e122c25c19b621
sunbang123 <93497158+sunbang123@users.noreply.github.com>
2026-06-25 13:29:02 +0900

    chore: MFC기반 Native UI 세팅

---
+++ Wafer-AOI-Project/WaferUINative/WaferUINative/WaferUINativeView.cpp
@@ -0,0 +1,128 @@
+﻿
+// WaferUINativeView.cpp: CWaferUINativeView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "WaferUINative.h"
+#endif
+
+#include "WaferUINativeDoc.h"
+#include "WaferUINativeView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CWaferUINativeView
+
+IMPLEMENT_DYNCREATE(CWaferUINativeView, CView)
+
+BEGIN_MESSAGE_MAP(CWaferUINativeView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CWaferUINativeView::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CWaferUINativeView 생성/소멸
+
+CWaferUINativeView::CWaferUINativeView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CWaferUINativeView::~CWaferUINativeView()
+{
+}
+
+BOOL CWaferUINativeView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CWaferUINativeView 그리기
+
+void CWaferUINativeView::OnDraw(CDC* /*pDC*/)
+{
+	CWaferUINativeDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CWaferUINativeView 인쇄
+
+
+void CWaferUINativeView::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CWaferUINativeView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CWaferUINativeView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CWaferUINativeView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+void CWaferUINativeView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CWaferUINativeView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CWaferUINativeView 진단
+
+#ifdef _DEBUG
+void CWaferUINativeView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CWaferUINativeView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CWaferUINativeDoc* CWaferUINativeView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CWaferUINativeDoc)));
+	return (CWaferUINativeDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CWaferUINativeView 메시지 처리기
